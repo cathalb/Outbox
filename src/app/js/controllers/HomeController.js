a640b13483f4d8e4311cf31630a99390018afe65
cathalb <cathalmb@gmail.com>
2015-08-20 18:19:03 +0100

    Step 5 +6

--- src/app/js/controllers/HomeController.js
+++ src/app/js/controllers/HomeController.js
@@ -1,6 +1,8 @@
 /**
  * Created by cathal on 8/20/15.
  */
-app.controller('HomeController', ['$scope', function($scope) {
-
+app.controller('HomeController', ['$scope', 'emails', function($scope, emails) {
+  emails.success(function(data) {
+      $scope.emails = data;
+  })
 }]);
